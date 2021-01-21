d0899defb0c18f22941e0cdbb68417a8125912a6
sunting <1642426991@qq.com>
2021-01-21 15:58:19 +0800

    完成了登录和注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -18,10 +18,50 @@ $(function() {
         pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
         // 校验两次密码是否一致
         repwd: function(value) {
-            var pwd = $(".reg-box [name=password]");
+            var pwd = $(".reg-box [name=password]").val();
             if (pwd !== value) {
                 return "两次密码不一致";
             }
         },
+    });
+
+    // 监听注册表单提交事件
+    $("#form_reg").on("submit", function(e) {
+        e.preventDefault();
+        // alert("1");
+        var data = {
+            username: $("#form_reg [name=username]").val(),
+            password: $("#form_reg [name=password]").val(),
+        };
+        $.post("/api/reguser", data, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg("注册成功,请登录!");
+            $("#link_login").click();
+        });
+    });
+    // 监听登录表单的提交事件
+    $("#form_login").submit(function(e) {
+        // 阻止默认提交行为
+        // alert("1");
+        e.preventDefault();
+        $.ajax({
+            url: "/api/login",
+            method: "POST",
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg("登录失败！");
+                }
+                layer.msg("登录成功！");
+                // 将登录成功得到的 token 字符串，保存到 localStorage 中
+                localStorage.setItem("token", res.token);
+                // 跳转到后台主页
+                window.location.href = "index.html";
+            },
+        });
     });
 });
